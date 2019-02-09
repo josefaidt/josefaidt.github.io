7fce3dba4c4719ea02e6bdb84ebb8c130005f298
josef <josef.aidt@gmail.com>
2019-02-09 12:16:55 -0600

    cleanup

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,4 @@
 import wrapPageElementWithTransition from 'helpers/wrapPage'
-// require('prismjs/plugins/line-numbers/prism-line-numbers.css')
-// require('prismjs/themes/prism-tomorrow.css')
 require('prismjs/themes/prism.css')
 
 // Page Transitions
