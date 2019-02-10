97398abcc0fee285aebeab474985c1ed1d7624b6
josef <josef.aidt@gmail.com>
2019-02-10 11:07:48 -0600

    analytics update

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,14 +78,6 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: 'UA-134211753-1',
-        // Puts tracking script in the head instead of the body
-        head: false,
-        anonymize: true,
-        respectDNT: true,
-        exclude: ['/public/static/**'],
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: 'josefaidt.me',
       },
     },
     {
