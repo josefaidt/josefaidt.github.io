611fffc2f1c5e9acb2b26d65a5544b90a9fce558
josef <josef.aidt@gmail.com>
2019-04-19 12:54:40 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -114,8 +114,5 @@ module.exports = {
       },
     },
     `gatsby-plugin-netlify`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
   ],
 }
