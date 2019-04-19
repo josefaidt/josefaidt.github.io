d654a715bb9b5f872e315e7f6d64dcd79e7eb625
josef <josef.aidt@gmail.com>
2019-04-19 14:35:57 -0500

    🚀

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    // `gatsby-plugin-sitemap`,
+    `gatsby-plugin-sitemap`,
     // `gatsby-plugin-offline`,
     `gatsby-transformer-json`,
     {
