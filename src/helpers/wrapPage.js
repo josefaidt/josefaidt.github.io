259cad9c2ed89f22f038fd83647dd45f5411ea30
josef <josef.aidt@gmail.com>
2019-04-19 21:45:53 -0500

    remove theme provider

--- src/helpers/wrapPage.js
+++ src/helpers/wrapPage.js
@@ -2,21 +2,7 @@ import React from 'react'
 import Transition from 'components/transition'
 import PropTypes from 'prop-types'
 
-const theme = {
-  red: '#D46A6A',
-  red4: '#dc8787',
-  rouge: '#c6797e',
-  almostblack: '#5a535b',
-  maxWidth: '1000px',
-}
-
-const ThemeContext = React.createContext(theme)
-
-const wrapPageElement = ({ element, props }) => (
-  <ThemeContext.Provider>
-    <Transition {...props}>{element}</Transition>
-  </ThemeContext.Provider>
-)
+const wrapPageElement = ({ element, props }) => <Transition {...props}>{element}</Transition>
 
 wrapPageElement.propTypes = {
   element: PropTypes.any,
