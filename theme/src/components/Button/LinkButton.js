6509347c3664b674a2118616ad78a12a46b0a20e
josef <josef.aidt@gmail.com>
2019-10-08 17:54:44 -0500

    compensate for theme changes, bump padding-top

--- theme/src/components/Button/LinkButton.js
+++ theme/src/components/Button/LinkButton.js
@@ -4,22 +4,23 @@ import styled, { ThemeContext } from 'styled-components'
 
 const StyledLink = styled(Link)`
   padding: 0.5rem 0.5rem;
-  padding-top: 0.65rem;
+  padding-top: 0.7rem;
   margin: 0.2rem;
   text-decoration: none;
   line-height: 1rem;
   border-radius: 0.2rem;
+  color: ${props => props.theme.primary};
 
   &.active,
   &.active:hover {
     filter: none;
-    background-color: ${props => props.theme.main.concat('DF')};
+    background-color: ${props => props.theme.primary.concat('DF')};
     transition: all 200ms;
     color: ${props => props.theme.background};
   }
   &:hover {
     filter: none;
-    background-color: ${props => props.theme.main.concat('DF')};
+    background-color: ${props => props.theme.primary.concat('DF')};
     transition: all 200ms;
     color: ${props => props.theme.background};
   }
