109f130f8147330557cafdd7e0cd95e00030e193
josef <josef.aidt@gmail.com>
2019-10-10 18:14:38 -0500

    add sentry

--- app/gatsby-config.js
+++ app/gatsby-config.js
@@ -63,6 +63,15 @@ const config = {
         ],
       },
     },
+    {
+      resolve: 'gatsby-plugin-sentry',
+      options: {
+        dsn: process.env.SENTRY_DSN,
+        // Optional settings, see https://docs.sentry.io/clients/node/config/#optional-settings
+        environment: process.env.NODE_ENV,
+        enabled: (() => ['production', 'stage'].indexOf(process.env.NODE_ENV) !== -1)(),
+      },
+    },
     `gatsby-plugin-netlify`,
   ],
 }
