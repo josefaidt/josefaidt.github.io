b20579b84d9db5bb87eef6f55f56c6f916e467dd
josef <josef.aidt@gmail.com>
2020-02-05 17:58:14 -0600

    Revert "fix: shorten height on break viewbox"
    This reverts commit af007caa185f92caabf2b84b555743a6992c3964.

--- theme/src/components/Markdown/break.js
+++ theme/src/components/Markdown/break.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 const Line = () => (
-  <svg viewBox="0 0 100 5" xmlns="http://www.w3.org/2000/svg">
+  <svg viewBox="0 0 100 10" xmlns="http://www.w3.org/2000/svg">
     <line stroke="#bbb" x1="0" x2="200" y1="0" y2="0" />
   </svg>
 )
 
 export const ShortLine = () => (
-  <svg viewBox="0 0 100 5" xmlns="http://www.w3.org/2000/svg">
+  <svg viewBox="0 0 100 10" xmlns="http://www.w3.org/2000/svg">
     <line stroke="#bbb" x1="0" x2="50" y1="0" y2="0" />
   </svg>
 )
