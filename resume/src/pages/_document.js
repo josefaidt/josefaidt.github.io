3affa303118de56c36636e79660942c451d6cc10
josefaidt <josef.aidt@gmail.com>
2020-04-09 10:38:17 -0500

    feat: add base resume structure

---
+++ resume/src/pages/_document.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Josefin+Sans&family=Open+Sans&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
---
+++ resume/src/pages/index.js
@@ -0,0 +1,110 @@
+import React from 'react'
+import fetch from 'node-fetch'
+import Card from '../components/Card'
+import projects from '../data/projects.json'
+import query from './_query'
+
+const HomePage = ({ repositories, projects }) => {
+  console.log(repositories, projects)
+  return (
+    <main>
+      <h1>Josef Aidt</h1>
+      <section aria-label="contact information">
+        <p>
+          <strong>Email:</strong> josef.aidt@gmail.com <br />
+          <strong>Website:</strong> josefaidt.dev <br />
+          <strong>Twitter:</strong> @josefaidt <br />
+          <strong>Twitch:</strong> twitch.tv/josefaidt <br />
+          <strong>Github:</strong> github.com/josefaidt <br />
+        </p>
+      </section>
+      <section>
+        <header>
+          <h2>Personal Projects</h2>
+          <p>Highlights from my personal work</p>
+        </header>
+        {projects.map((project, i) => (
+          <Card key={i}>
+            <h4>{project.name}</h4>
+            <p>
+              <a href={project.url}>
+                {project.urlLabel || project.url.replace(/https?:\/\//, '')} &rarr;
+              </a>
+            </p>
+            <p>{project.description}</p>
+            <div className="card--technologies">
+              {project.technologies.map((technology, k) => (
+                <div key={k}>{technology}</div>
+              ))}
+            </div>
+          </Card>
+        ))}
+      </section>
+      <section>
+        <header>
+          <h2>Work Experience</h2>
+        </header>
+        <article>
+          <header>
+            <h3>Full-Stack JavaScript Developer at IBM</h3>
+            <p>Baton Rouge, LA</p>
+            <p>December 2018 - Present</p>
+          </header>
+          <ul>
+            <li>Established local front-end development group chapter, leads bi-weekly meetups</li>
+            <li>Build learning management system (Node.js, React)</li>
+          </ul>
+        </article>
+        <article>
+          <header>
+            <h3>Business Intelligence Developer at FMOL Health System</h3>
+            <p>Baton Rouge, LA</p>
+            <p>August 2017 - December 2018</p>
+          </header>
+          <ul>
+            <li>Build and support Lawson ERP submodules (HTML5, CSS3, JavaScript)</li>
+            <li>
+              Design financial, human resources, and logistics reports (Oracle SQL, Microsoft SQL,
+              Tableau)
+            </li>
+          </ul>
+        </article>
+        <article>
+          <header>
+            <h3>IS Support Analyst at FMOL Health System</h3>
+            <p>Baton Rouge, LA</p>
+            <p>October 2013 - August 2017</p>
+          </header>
+          <ul>
+            <li>Lead large-scale endpoint deployments</li>
+            <li>
+              Buid inventory receiving application infrastructure (MySQL, Microsoft Server, PHP)
+            </li>
+          </ul>
+        </article>
+      </section>
+    </main>
+  )
+}
+
+export async function getStaticProps(context) {
+  const getGithubData = async () => {
+    const res = await fetch('https://api.github.com/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+      },
+      body: JSON.stringify({ query }),
+    })
+    const data = await res.json()
+    return data?.data
+  }
+  const githubData = await getGithubData()
+  return {
+    props: { projects, repositories: githubData.user.repositories }, // will be passed to the page component as props
+  }
+}
+
+export default HomePage
