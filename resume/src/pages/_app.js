c09200b44c80ea361e857ec213396e76fbd44bee
josefaidt <josef.aidt@gmail.com>
2020-04-14 16:29:58 -0500

    feat(resume): add seo

--- resume/src/pages/_app.js
+++ resume/src/pages/_app.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Head } from 'next/head'
 import NotificationContainer from '../components/NotificationContainer'
 import { NotificationProvider } from '../helpers/NotificationContext'
 import '../global.css'
